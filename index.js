079e9689c416ea964c4c2d454b1a3063f8378844
axel-mnd <75190608+Ima0ne10@users.noreply.github.com>
2021-10-04 17:40:50 +0200

    question 8

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+// const { svg } = require("./d3");
 
+d3.select("svg").style("background-color", "purple");
+
+
+d3.selectAll("p").style("font-size", "30px");
+
+d3.select("svg").on("mousemove", function(e){
+    let p = d3.pointer(e);
+    d3.select("image").attr("x", p[0])
+        .attr("y", p[1])
+        .style("transform","translateX(-7px) translateY(-7px)")
+})
+
+let tab=[];
+
+setInterval(function(){
+
+    let aleat_y = Math.floor(Math.random()*100);
+
+    d3.select("svg").append("image")
+        .attr("xlink:href","images/cat.png")
+        .attr("x", "100")
+        .attr("y", aleat_y)
+        .attr("width", "15px")
+        .attr("class", "cat")
+    
+    var x_cat = parseInt(d3.select(".cat").attr("x"));
+    var y_cat = aleat_y;
+
+    tab.push([x_cat,y_cat]);
+
+}, 1000)
+
+
+tab.forEach( )
+while(x_cat>0){
+    x_cat = x_cat - 10;
+    d3.select(".cat").attr("x", x_cat);
+}
